d7bfe3e01d2a6c3c012cd53ff9c17f3a0f0c35f3
Mohamed Tribak <mohamed.tribak@gmail.com>
2020-03-10 14:10:52 -0700

    update server js

--- server.js
+++ server.js
@@ -1,8 +1,1 @@
-git init
-git remote add origin https://github.com/TrimedGit/Trimed1.git
-git fetch
-git add .
-git commit -a -m "1 init"
-    git config --global user.email "mohamed.tribak@gmail.com"
-    git config --global user.name "Med Tribak"
-git rebase origin/master
+ddd

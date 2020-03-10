60126e944ab80e8508f128bbe6138a7248b2ff97
M <mohamed.tribak@gmail.com>
2020-03-10 13:43:27 -0700

    1 init

---
+++ server.js
@@ -0,0 +1,7 @@
+git init
+git remote add origin https://github.com/TrimedGit/Trimed1.git
+git fetch
+git add .
+git commit -a -m "1 init"
+    git config --global user.email "mohamed.tribak@gmail.com"
+    git config --global user.name "Med Tribak"
